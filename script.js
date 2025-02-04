92e2b1a06bb15cca15e70cf8146e41991398c645
pietereb <pieterthecoder@proton.me>
2025-02-04 17:39:49 +0100

    Finish coding logic
    Game is now fully operational with all three choices (rock, paper
    and scissors), and will play until either play reaches 3 points.
    Upon 3 points for either player, user can reset the game.

--- script.js
+++ script.js
@@ -3,47 +3,29 @@ let userScore = 0;
 let computerScore = 0;
 
 // Store display div's in variables
+let currentResults = document.querySelector("#current");
 let userScoreDisplay = document.querySelector("#usrscore");
 let computerScoreDisplay = document.querySelector("#cmpscore");
 let finalScoreDisplay = document.querySelector("#finalscore");
 
-//Function to have computer generate Rock, Paper or Scissors
+// Function to have computer generate Rock, Paper or Scissors
 let getComputerChoice = function(){
     let randomNumber = Math.random()*3
     if (randomNumber < 1) {
-        let computerChoice = "Rock"
-        return computerChoice
+        let computerChoice = "Rock";
+        return computerChoice;
     } else if (randomNumber > 2) {
-        let computerChoice = "Scissors"
-        return computerChoice
+        let computerChoice = "Scissors";
+        return computerChoice;
     } else {
-        let computerChoice = "Paper"
-        return computerChoice
-    }
-}
+        let computerChoice = "Paper";
+        return computerChoice;
+    };
+};
 
-// User selects Rock
-let rockBtn = document.querySelector("#rock");
-rockBtn.addEventListener("click", () =>{
-    if (userScore < 3 && computerScore < 3){
-        let userChoice = "Rock";
-        console.log(`You selected ${userChoice}.`);
-
-        let computerChoice = getComputerChoice();
-        console.log(`The computer selected ${computerChoice}.`)
-
-        if (computerChoice == "Paper"){
-            console.log("You lost.");
-            computerScore++;
-        } else if (computerChoice == "Scissors"){
-            console.log("You won!");
-            userScore++;
-        } else {
-            console.log("It's a tie...");
-        }
-
-        userScoreDisplay.textContent = `Your score is: ${userScore}`;
-
+// Function to display scores
+let displayScores = function(){
+    userScoreDisplay.textContent = `Your score is: ${userScore}`;
         computerScoreDisplay.textContent = `The computer score is: ${computerScore}`;
 
         if (userScore >=3){
@@ -55,21 +37,103 @@ rockBtn.addEventListener("click", () =>{
             console.log("The computer beat you 3 out of 5, better luck next time! Click the reset button to play another time.");
             finalScoreDisplay.textContent = "The computer beat you 3 out of 5, better luck next time! Click the reset button to play another time.";
         };
-    };
-});
+};
 
+// Function to reset game
 let resetGame = function(){
     userScore = 0;
     computerScore = 0;
+    currentResults.textContent = "";
+    userScoreDisplay.textContent = "";
+    computerScoreDisplay.textContent = "";
+    finalScoreDisplay.textContent = "";
 };
 
+// User selects Rock
+let rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () =>{
+    if (userScore < 3 && computerScore < 3){
+        let userChoice = "Rock";
+        let computerChoice = getComputerChoice();
+
+        if (computerChoice == "Paper"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You lost.`;
+            computerScore++;
+        } else if (computerChoice == "Scissors"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You won!`;
+            userScore++;
+        } else {
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            It's a tie...`;
+        };
+
+        displayScores();
+    };
+});
+
+// User selects Paper
+let paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", () =>{
+    if (userScore < 3 && computerScore < 3){
+        let userChoice = "Paper";
+        let computerChoice = getComputerChoice();
+
+        if (computerChoice == "Scissors"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You lost.`;
+            computerScore++;
+        } else if (computerChoice == "Rock"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You won!`;
+            userScore++;
+        } else {
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            It's a tie...`;
+        };
+
+        displayScores();
+    };
+});
+
+// User selects Rock
+let scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", () =>{
+    if (userScore < 3 && computerScore < 3){
+        let userChoice = "Scissors";
+        let computerChoice = getComputerChoice();
+
+        if (computerChoice == "Rock"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You lost.`;
+            computerScore++;
+        } else if (computerChoice == "Paper"){
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            You won!`;
+            userScore++;
+        } else {
+            currentResults.textContent = `You selected ${userChoice}.
+            The computer selected ${computerChoice}.
+            It's a tie...`;
+        };
+
+        displayScores();
+    };
+});
+
+// Allow user to reset game after 3 out of 5
 let resetBtn = document.querySelector("#reset");
 resetBtn.addEventListener("click", () => {
     if (userScore >= 3 || computerScore >= 3){
     resetGame();
-    userScoreDisplay.textContent = "";
-    computerScoreDisplay.textContent = "";
-    finalScoreDisplay.textContent = "";
-    console.log("Game was reset to default.");
     };
 });
